aabea34c0721d166102bbf3182e7b7317725802d
Tyler Bromley <tylerbromley@Tylers-MacBook-Air.local>
2021-06-19 10:52:22 -0700

    bunch of field, body, path, query params stuff

--- main.py
+++ main.py
@@ -1,31 +1,60 @@
 from enum import Enum
-from typing import Optional
+from typing import List, Optional, Set
 
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import Body, FastAPI, Path, Query
+from pydantic import BaseModel, Field, HttpUrl
+
+class Image(BaseModel):
+    url: HttpUrl
+    name: str
 
 class Item(BaseModel):
     name: str
-    description: Optional[str] = None
-    price: float
+    description: Optional[str] = Field(
+        None, title="The description of the item", max_length=300
+    )
+    price: float = Field(..., gt=0, description="The price must be greater than zero")
     tax: Optional[float] = None
+    tags: Set[str] = set()
+    image: Optional[List[Image]] = None
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Foo",
+                "description": "A very nice Item",
+                "price": 35.4,
+                "tax": 3.2,
+                "tags": [],
+                "image": [
+                    {
+                        "url": "string",
+                        "name": "string"
+                    }
+                ]
+            }
+        }
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
     lenet = "lenet"
 
+class Offer(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    items: List[Item]
+
+class User(BaseModel):
+    username: str
+    full_name: Optional[str] = None
+
+
+
 app = FastAPI()
 
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
-
-@app.get("/")
-async def root():
-    return {"message": "Welcome Home!"}
-
-@app.get("/users/me")
-async def read_user_me():
-    return {"user_id": "the current user"}
 
 """
 Declaring other function params that are not part of the path params,
@@ -37,10 +66,45 @@ knows which is which. No specific order is required.
 
 If query param required, don't declare a default value
 """
+######## GET ########
 
+@app.get("/")
+async def root():
+    return {"message": "Welcome Home!"}
+
+# Optional[str] = Query(None, some_other_arg) makes Query optional,
+# using ... as the first arg in query means it is required
 @app.get("/items/")
-async def read_item(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip: skip + limit]
+async def read_items(
+    q: Optional[str] = Query(
+        None,
+        title="Query string",
+        min_length=3,
+        max_length=50,
+        description="Query string for the items to search in the database " +
+        "that have a good match",
+        # regex="^fixedquery$",
+    )
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+# the function param q will be optional, and None by default
+@app.get("/items/{item_id}")
+async def read_item_id(
+    item_id: int = Path(..., title="The ID of the item to get"),
+    q: Optional[str] = Query(None, alias="item-query")
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
 
 @app.get("/users/{user_id}/items/{items_id}")
 async def read_user_item(
@@ -55,12 +119,7 @@ async def read_user_item(
         )
     return item
 
-# the function param q will be optional, and None by default
-@app.get("/items/{item_id}")
-async def read_item_id(item_id: str, q: Optional[str] = None):
-    if q:
-        return {"item_id": item_id, "q": q}
-    return {"item_id": item_id}
+
 
 @app.get("/models/{model_name}")
 # Declare path parameter with a type annotation using the enum class ModelName
@@ -76,6 +135,12 @@ async def get_model(model_name: ModelName):
 async def read_file(file_path: str):
     return {"file_path": file_path}
 
+######## POST ########
+
+@app.post("/images/multiple/")
+async def create_multiple_images(images: List[Image]):
+    return images
+
 @app.post("/items/")
 async def create_item(item: Item):
     # update the model with price + tax
@@ -85,11 +150,17 @@ async def create_item(item: Item):
         item_dict.update({"price_with_tax": price_with_tax})
     return item_dict
 
+@app.post("/offers")
+async def create_offer(offer: Offer):
+    return offer
+
+
+
+######## PUT ########
+
 # Declare body, path and query params, all at the same time.
-@app.put("/items/{item_id")
-async def create_item(item_id: int, item: Item, q: Optional[str] = None):
-    result = {"item_id": item_id, **item.dict()}
-    if q:
-        result.update({"q": q})
-    return result
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    results = {"item_id": item_id, "item": item}
+    return results
 
