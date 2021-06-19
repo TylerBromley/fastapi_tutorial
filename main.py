f9455dd0a0099015c06d7df32a2a628afe01956a
Tyler Bromley <tylerbromley@Tylers-MacBook-Air.local>
2021-06-19 09:20:04 -0700

    pydantic and request bodies

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from enum import Enum
 from typing import Optional
 
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
@@ -68,3 +75,21 @@ async def get_model(model_name: ModelName):
 @app.get("/files/{file_path:path}")
 async def read_file(file_path: str):
     return {"file_path": file_path}
+
+@app.post("/items/")
+async def create_item(item: Item):
+    # update the model with price + tax
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+# Declare body, path and query params, all at the same time.
+@app.put("/items/{item_id")
+async def create_item(item_id: int, item: Item, q: Optional[str] = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
+
